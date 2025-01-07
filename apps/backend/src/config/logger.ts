a3853d4b6a001cfcbe45a1b9090c5d6a4afefb2f
Phutsakorn Thunwattanakul <p.thunwattanakul@gmail.com>
2025-01-08 01:58:51 +0700

    chore: fix docker compose

--- apps/backend/src/config/logger.ts
+++ apps/backend/src/config/logger.ts
@@ -2,33 +2,34 @@ import { createPinoLogger, pino } from "@bogeychan/elysia-logger";
 import { config } from "./env";
 
 export const logger = createPinoLogger({
-  transport: {
-    targets: [
-      { target: "pino-pretty", 
-        options: {
-        colorize: true,
-        },
-      }, 
-      { target: "pino/file" },
-      {
-      target: 'pino-elasticsearch',
-      options: {
-        index: 'app-log-index',
-        node: `${config.ELASTICSEARCH_URL}:${config.ELASTICSEARCH_PORT}`,
-        esVersion: 7,
-        flushBytes: 1000
-        }
-      }
-    ]
-  },
-  // FOR: print all information about api
-  // formatters: {
-  //   log: (object) => ({
-  //     ...object,
-  //   }),
-  // },
-  level: "debug",
-  timestamp: pino.stdTimeFunctions.isoTime
+	transport: {
+		targets: [
+			{
+				target: "pino-pretty",
+				options: {
+					colorize: true,
+				},
+			},
+			{ target: "pino/file" },
+			{
+				target: "pino-elasticsearch",
+				options: {
+					index: "app-log-index",
+					node: `${config.ELASTICSEARCH_URL}:${config.ELASTICSEARCH_PORT}`,
+					esVersion: 7,
+					flushBytes: 1000,
+				},
+			},
+		],
+	},
+	// FOR: print all information about api
+	// formatters: {
+	//   log: (object) => ({
+	//     ...object,
+	//   }),
+	// },
+	level: "debug",
+	timestamp: pino.stdTimeFunctions.isoTime,
 });
 
 export const postgresLogger = logger.child({ service: "postgres" });
--- apps/backend/src/infrastructures/redis/connection.ts
+++ apps/backend/src/infrastructures/redis/connection.ts
@@ -3,25 +3,25 @@ import { redisLogger } from "@/config/logger";
 import { createClient } from "redis";
 
 export const redis = createClient({
-  url: `redis://${config.REDIS_HOST}:${config.REDIS_PORT}/${
-    config.REDIS_DBINDEX || 0
-  }`,
+	url: `redis://${config.REDIS_HOST}:${config.REDIS_PORT}/${
+		config.REDIS_DB_INDEX || 0
+	}`,
 });
 
 redis.on("error", (error) => {
-  redisLogger.error(`❌  Redis client error: ${JSON.stringify(error)}`);
+	redisLogger.error(`❌  Redis client error: ${JSON.stringify(error)}`);
 });
 
 export const connectRedis = async (): Promise<void> => {
-  try {
-    await redis.connect();
-    redisLogger.info("✅  Connect redis success");
-  } catch (e) {
-    redisLogger.error(
-      `❌  Connect redis (redis://${config.REDIS_HOST}:${config.REDIS_PORT}/${
-        config.REDIS_DBINDEX || 0
-      }) failed: ${e}`,
-      e
-    );
-  }
+	try {
+		await redis.connect();
+		redisLogger.info("✅  Connect redis success");
+	} catch (e) {
+		redisLogger.error(
+			`❌  Connect redis (redis://${config.REDIS_HOST}:${config.REDIS_PORT}/${
+				config.REDIS_DB_INDEX || 0
+			}) failed: ${e}`,
+			e,
+		);
+	}
 };
