762b6404d2da3b837bc71bd867f75700023648fe
Phutsakorn Thunwattanakul <p.thunwattanakul@gmail.com>
2025-01-10 14:02:31 +0700

    feat: new migration db

--- apps/backend/src/applications/controller/userController.ts
+++ apps/backend/src/applications/controller/userController.ts
@@ -1,12 +1,11 @@
-import { userModel } from "@/domains/models/user";
 import { Elysia, t } from "elysia";
 
 export const UserController = new Elysia({ prefix: "/users" }).post(
-  "/:id",
-  async ({ body, cookie: { session } }) => {
-    return true;
-  },
-  {
-    body: t.Object(userModel.insert.user),
-  }
+	"/:id",
+	async ({ body, cookie: { session } }) => {
+		return true;
+	},
+	{
+		body: t.Object({ email: t.String() }),
+	},
 );
