e6813af5c479697ee8c528e658f2763e47f45f29
Phutsakorn Thunwattanakul <p.thunwattanakul@gmail.com>
2025-01-10 14:09:39 +0700

    fix: seed script

--- apps/backend/src/infrastructures/database/seed.ts
+++ apps/backend/src/infrastructures/database/seed.ts
@@ -4,47 +4,47 @@ import { cleanupDB } from "@/utils/db-utils";
 import { db } from "./connection";
 
 export async function seedAuth(): Promise<void> {
-  logger.info("🌱 Seeding auth data...");
-  await db.transaction(async (context) => {
-    await cleanupDB(context, user);
-    await cleanupDB(context, session);
-    await cleanupDB(context, verification);
-    await cleanupDB(context, account);
-    logger.info("🧹 Cleaned up the database...");
+	logger.info("🌱 Seeding auth data...");
+	await db.transaction(async (context) => {
+		await cleanupDB(context, user);
+		await cleanupDB(context, session);
+		await cleanupDB(context, verification);
+		await cleanupDB(context, account);
+		logger.info("🧹 Cleaned up the database...");
 
-    const sampleUsers = [
-      {
-        id: "1",
-        name: "John Doe",
-        email: "user1@user.com",
-        emailVerified: true,
-        image: "http://example.com/image.jpg",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        role: "user",
-      },
-      {
-        id: "2",
-        name: "Jane Smith",
-        email: "user2@user.com",
-        emailVerified: false,
-        image: null,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        role: "user",
-      },
-      {
-        id: "3",
-        name: "Robert crate",
-        email: "admin@admin.com",
-        emailVerified: false,
-        image: null,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        role: "user",
-      },
-    ];
-
-    logger.info("🌱 Auth data has been seeded");
-  });
+		const sampleUsers = [
+			{
+				id: "1",
+				name: "John Doe",
+				email: "user1@user.com",
+				emailVerified: true,
+				image: "http://example.com/image.jpg",
+				createdAt: new Date(),
+				updatedAt: new Date(),
+				role: "user",
+			},
+			{
+				id: "2",
+				name: "Jane Smith",
+				email: "user2@user.com",
+				emailVerified: false,
+				image: null,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+				role: "user",
+			},
+			{
+				id: "3",
+				name: "Robert crate",
+				email: "admin@admin.com",
+				emailVerified: false,
+				image: null,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+				role: "user",
+			},
+		];
+		await context.insert(user).values(sampleUsers);
+		logger.info("🌱 Auth data has been seeded");
+	});
 }
--- apps/backend/src/utils/db-utils.ts
+++ apps/backend/src/utils/db-utils.ts
@@ -1,24 +1,28 @@
 import { type AnyColumn, type TableConfig, sql } from "drizzle-orm";
 import type {
-  PgQueryResultHKT,
-  PgTableWithColumns,
-  PgTransaction,
+	PgQueryResultHKT,
+	PgTableWithColumns,
+	PgTransaction,
 } from "drizzle-orm/pg-core";
 
 export async function cleanupDB<T extends TableConfig>(
-  schema: PgTransaction<PgQueryResultHKT, Record<string, unknown>>,
-  table: PgTableWithColumns<T>
+	schema: PgTransaction<
+		PgQueryResultHKT,
+		Record<string, unknown>,
+		Record<string, any>
+	>,
+	table: PgTableWithColumns<T>,
 ): Promise<void> {
-  await schema.delete(table);
+	await schema.delete(table);
 }
 
 export const customCount = (column?: AnyColumn) => {
-  if (column) {
-    return sql<number>`cast(count(${column}) as integer)`;
-  }
-  return sql<number>`cast(count(*) as integer)`;
+	if (column) {
+		return sql<number>`cast(count(${column}) as integer)`;
+	}
+	return sql<number>`cast(count(*) as integer)`;
 };
 
 export const getCount = (data: { count: number }[]) => {
-  return data[0]?.count ?? 0;
+	return data[0]?.count ?? 0;
 };
