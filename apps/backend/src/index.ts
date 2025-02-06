5eb1ee354d50ae85d75290af372cbd490c7e1feb
cinnamonjs <p.thunwattanakul@gmail.com>
2025-02-06 21:22:14 +0700

    feat: aws-cli

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,5 @@
 import { augmentation } from "@/applications/controllers/augmentationController";
+import { dataset } from "@/applications/controllers/datasetController";
 import { config } from "@/config/env";
 import { logger } from "@/config/logger";
 import { swaggerConfig } from "@/config/swagger";
@@ -7,50 +8,49 @@ import { connectRabbitMQ } from "@/infrastructures/rabbitmq/connection";
 import { connectRedis } from "@/infrastructures/redis/connection";
 import { connectS3 } from "@/infrastructures/s3/connection";
 import { betterAuthView } from "@/lib/auth";
+import { retryConnection } from "@/utils/retry";
 import { shutdown } from "@/utils/shutdown";
 import { cors } from "@elysiajs/cors";
 import { staticPlugin } from "@elysiajs/static";
 import { Elysia } from "elysia";
-import { retryConnection } from "@/utils/retry";
-import { dataset } from "@/applications/controllers/datasetController";
 
 try {
-  logger.info("🏃‍♀️ Starting connection..");
-  await connectRedis();
-  await retryConnection(connectRabbitMQ, "RabbitMQ");
-  await connectDatabase();
-  await connectS3();
-  logger.info("🏃‍♀️ Starting server..");
+	logger.info("🏃‍♀️ Starting connection..");
+	await connectRedis();
+	await retryConnection(connectRabbitMQ, "RabbitMQ", 10);
+	await connectDatabase();
+	await retryConnection(connectS3, "S3", 10);
+	logger.info("🏃‍♀️ Starting server..");
 
-  const app = new Elysia()
-    .use(logger.into())
-    .use(cors())
-    .use(staticPlugin())
-    .use(swaggerConfig())
-    .use(augmentation)
-    .use(dataset)
-    .onParse(({ request, route }) => {
-      if (route.startsWith("/api/auth")) {
-        return request.body;
-      }
-    })
-    .all("/api/auth/*", betterAuthView)
-    .get("/", () => "CustomAI API")
-    .get("/route-count", () => {
-      const routeCount = Object.keys(app.routes).length;
-      const routeLength = app.routes.length;
-      return { routes: routeCount, length: routeLength };
-    })
-    .onStop(shutdown)
-    .listen(config.APP_PORT);
+	const app = new Elysia()
+		.use(logger.into())
+		.use(cors())
+		.use(staticPlugin())
+		.use(swaggerConfig())
+		.use(augmentation)
+		.use(dataset)
+		.onParse(({ request, route }) => {
+			if (route.startsWith("/api/auth")) {
+				return request.body;
+			}
+		})
+		.all("/api/auth/*", betterAuthView)
+		.get("/", () => "CustomAI API")
+		.get("/route-count", () => {
+			const routeCount = Object.keys(app.routes).length;
+			const routeLength = app.routes.length;
+			return { routes: routeCount, length: routeLength };
+		})
+		.onStop(shutdown)
+		.listen(config.APP_PORT);
 
-  process.on("SIGINT", app.stop);
-  process.on("SIGTERM", app.stop);
+	process.on("SIGINT", app.stop);
+	process.on("SIGTERM", app.stop);
 
-  logger.info(
-    `🦊  Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-  );
+	logger.info(
+		`🦊  Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	);
 } catch (e) {
-  logger.error(e, "🚫  Error booting the server");
-  process.exit();
+	logger.error(e, "🚫  Error booting the server");
+	process.exit();
 }
