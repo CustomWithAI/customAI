0f38be1422b19435dcd26d11fe8b46eef29ff08a
PPHamster <shinnapatjr@gmail.com>
2025-03-19 03:50:49 +0700

    fix: split data version 2 (not stable)

--- apps/backend/src/utils/split-data.ts
+++ apps/backend/src/utils/split-data.ts
@@ -1,13 +1,76 @@
-type ClassImageData = {
-  class: string;
+interface ClassificationAnnotation {
+  label: string;
+}
+
+interface ObjectDetectionAnnotation {
+  annotation: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    label: string;
+  }[];
+}
+
+interface SegmentationAnnotation {
+  annotation: {
+    points: { x: number; y: number }[];
+    label: string;
+  }[];
+}
+
+interface ImageData {
+  url: string;
+  annotation:
+    | ClassificationAnnotation
+    | ObjectDetectionAnnotation
+    | SegmentationAnnotation;
+}
+
+function isClassificationAnnotation(
+  annotation:
+    | ClassificationAnnotation
+    | ObjectDetectionAnnotation
+    | SegmentationAnnotation
+): annotation is ClassificationAnnotation {
+  return (annotation as ClassificationAnnotation).label !== undefined;
+}
+
+function isObjectDetectionAnnotation(
+  annotation:
+    | ClassificationAnnotation
+    | ObjectDetectionAnnotation
+    | SegmentationAnnotation
+): annotation is ObjectDetectionAnnotation {
+  return (annotation as ObjectDetectionAnnotation).annotation !== undefined;
+}
+
+function isSegmentationAnnotation(
+  annotation:
+    | ClassificationAnnotation
+    | ObjectDetectionAnnotation
+    | SegmentationAnnotation
+): annotation is SegmentationAnnotation {
+  return (
+    (annotation as SegmentationAnnotation).annotation !== undefined &&
+    Array.isArray((annotation as SegmentationAnnotation).annotation[0].points)
+  );
+}
+
+type SplitResult<T> = {
+  train: T[];
+  test: T[];
+  valid: T[];
 };
 
-function defaultSampling<T extends ClassImageData>(
+function defaultSplit<T extends ImageData>(
   dataset: T[],
   trainRatio: number,
   testRatio: number,
-  validRatio: number
-) {
+  validRatio: number,
+  labels: string[],
+  type: "classification" | "objectDetection" | "segmentation"
+): SplitResult<T> {
   const result = {
     train: [] as T[],
     test: [] as T[],
@@ -15,15 +78,39 @@ function defaultSampling<T extends ClassImageData>(
   };
 
   const groupedData: { [key: string]: T[] } = {};
+
   for (const data of dataset) {
-    if (!groupedData[data.class]) {
-      groupedData[data.class] = [];
+    let dataLabels: string[] = [];
+
+    if (
+      type === "classification" &&
+      isClassificationAnnotation(data.annotation)
+    ) {
+      dataLabels = [data.annotation.label];
+    } else if (
+      type === "objectDetection" &&
+      isObjectDetectionAnnotation(data.annotation)
+    ) {
+      dataLabels = data.annotation.annotation.map((item) => item.label);
+    } else if (
+      type === "segmentation" &&
+      isSegmentationAnnotation(data.annotation)
+    ) {
+      dataLabels = data.annotation.annotation.map((item) => item.label);
     }
-    groupedData[data.class].push(data);
+
+    for (const label of dataLabels) {
+      if (labels.includes(label)) {
+        if (!groupedData[label]) {
+          groupedData[label] = [];
+        }
+        groupedData[label].push(data);
+      }
+    }
   }
 
-  for (const classKey of Object.keys(groupedData)) {
-    const classData = groupedData[classKey];
+  for (const label of Object.keys(groupedData)) {
+    const classData = groupedData[label];
     const total = classData.length;
     const trainCount = Math.floor(total * trainRatio);
     const testCount = Math.floor(total * testRatio);
@@ -36,12 +123,14 @@ function defaultSampling<T extends ClassImageData>(
   return result;
 }
 
-function stratifiedSampling<T extends ClassImageData>(
+function stratifiedSplit<T extends ImageData>(
   dataset: T[],
   trainRatio: number,
   testRatio: number,
-  validRatio: number
-) {
+  validRatio: number,
+  labels: string[],
+  type: "classification" | "objectDetection" | "segmentation"
+): SplitResult<T> {
   const result = {
     train: [] as T[],
     test: [] as T[],
@@ -51,18 +140,41 @@ function stratifiedSampling<T extends ClassImageData>(
   const groupedData: { [key: string]: T[] } = {};
 
   for (const data of dataset) {
-    if (!groupedData[data.class]) {
-      groupedData[data.class] = [];
+    let dataLabels: string[] = [];
+
+    if (
+      type === "classification" &&
+      isClassificationAnnotation(data.annotation)
+    ) {
+      dataLabels = [data.annotation.label];
+    } else if (
+      type === "objectDetection" &&
+      isObjectDetectionAnnotation(data.annotation)
+    ) {
+      dataLabels = data.annotation.annotation.map((item) => item.label);
+    } else if (
+      type === "segmentation" &&
+      isSegmentationAnnotation(data.annotation)
+    ) {
+      dataLabels = data.annotation.annotation.map((item) => item.label);
     }
-    groupedData[data.class].push(data);
+
+    for (const label of labels) {
+      if (labels.includes(label)) {
+        if (!groupedData[label]) {
+          groupedData[label] = [];
+        }
+        groupedData[label].push(data);
+      }
+    }
   }
 
   const minClassCount = Math.min(
     ...Object.values(groupedData).map((items) => items.length)
   );
 
-  for (const classKey of Object.keys(groupedData)) {
-    const classData = groupedData[classKey];
+  for (const label of Object.keys(groupedData)) {
+    const classData = groupedData[label];
     const total = minClassCount;
 
     const shuffledData = shuffle(classData).slice(0, total);
@@ -78,7 +190,7 @@ function stratifiedSampling<T extends ClassImageData>(
   return result;
 }
 
-function shuffle<T extends ClassImageData>(array: T[]): T[] {
+function shuffle<T>(array: T[]): T[] {
   const shuffledArray = [...array];
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
